a2dfea9df1da05ba807fb03b9d1b7134e871d6ea
M.K.Thiong'o <kamauthiongo58@gmail.com>
2025-10-24 18:38:03 +0300

    update fixed changes to components

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="bg-orange-300 text-center py-4 text-rose-700 font-medium border-t border-rose-200">
-      © {new Date().getFullYear()} Uber Eat. All rights reserved.
+      @ {new Date().getFullYear()} Uber Eat. All rights reserved.
     </footer>
   );
 };
